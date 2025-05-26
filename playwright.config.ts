fa289dcd8e811b897a473812920d95d15fc36dfe
CO Tuan Tran <co-ttran@CGSCANADA.cgsinc.local>
2025-05-26 14:16:30 -0300

    fix playwright structure

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,30 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
+/**
+* Read environment variables from file.
+* https://github.com/motdotla/dotenv
+*/
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
   testDir: './tests',
+  reporter: [['html', { host: '0.0.0.0', open: 'never' }]],
   use: {
-    baseURL: 'http://localhost:3000'
-  }
+    screenshot: 'on',
+    video: 'on',
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+ 
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    ignoreHTTPSErrors: true,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ]
 });
